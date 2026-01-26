08dcb4e47bd78b7ad8011ba1667d1a92b6aca41c
LiegeSphy <muhammadzakywijaya01@gmail.com>
2026-01-26 12:18:53 +0800

    script-ed changes

---
+++ script.js
@@ -0,0 +1,167 @@
+var modprj1 = document.getElementById("modproj1");
+var prodesc1 = document.getElementById("prodesc1");
+var span1 = document.getElementsByClassName("close1")[0];
+
+prodesc1.onclick = function() {
+  modprj1.style.display = "block";
+}
+
+span1.onclick = function() {
+  modprj1.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modprj1) {
+    modprj1.style.display = "none";
+  }
+}
+
+
+var modprj2 = document.getElementById("modproj2");
+var prodesc2 = document.getElementById("prodesc2");
+var span2 = document.getElementsByClassName("close2")[0];
+
+prodesc2.onclick = function() {
+  modprj2.style.display = "block";
+}
+
+span2.onclick = function() {
+  modprj2.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modprj2) {
+    modprj2.style.display = "none";
+  }
+}
+
+
+var modprj3 = document.getElementById("modproj3");
+var prodesc3 = document.getElementById("prodesc3");
+var span3 = document.getElementsByClassName("close3")[0];
+
+prodesc3.onclick = function() {
+  modprj3.style.display = "block";
+}
+
+span3.onclick = function() {
+  modprj3.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modprj3) {
+    modprj3.style.display = "none";
+  }
+}
+
+// ----------------------------------PROJECT SECTION---------------------------------------- //
+// ^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^ //
+// ----------------------------------PROFILE SECTION---------------------------------------- //
+
+var modprf1 = document.getElementById("modprof1");
+var profdesc1 = document.getElementById("profdesc1");
+var spanprof1 = document.getElementsByClassName("closeprof1")[0];
+
+profdesc1.onclick = function() {
+  modprf1.style.display = "block";
+}
+
+spanprof1.onclick = function() {
+  modprf1.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modprf1) {
+    modprf1.style.display = "none";
+  }
+}
+
+var modprf2 = document.getElementById("modprof2");
+var profdesc2 = document.getElementById("profdesc2");
+var spanprof2 = document.getElementsByClassName("closeprof2")[0];
+
+profdesc2.onclick = function() {
+  modprf2.style.display = "block";
+}
+
+spanprof2.onclick = function() {
+  modprf2.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modprf2) {
+    modprf1.style.display = "none";
+  }
+}
+
+var modprf3 = document.getElementById("modprof3");
+var profdesc3 = document.getElementById("profdesc3");
+var spanprof3 = document.getElementsByClassName("closeprof3")[0];
+
+profdesc3.onclick = function() {
+  modprf3.style.display = "block";
+}
+
+spanprof3.onclick = function() {
+  modprf3.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modprf3) {
+    modprf1.style.display = "none";
+  }
+}
+
+var modprf4 = document.getElementById("modprof4");
+var profdesc4 = document.getElementById("profdesc4");
+var spanprof4 = document.getElementsByClassName("closeprof4")[0];
+
+profdesc4.onclick = function() {
+  modprf4.style.display = "block";
+}
+
+spanprof4.onclick = function() {
+  modprf4.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modprf4) {
+    modprf1.style.display = "none";
+  }
+}
+
+var modprf5 = document.getElementById("modprof5");
+var profdesc5 = document.getElementById("profdesc5");
+var spanprof5 = document.getElementsByClassName("closeprof5")[0];
+
+profdesc5.onclick = function() {
+  modprf5.style.display = "block";
+}
+
+spanprof5.onclick = function() {
+  modprf5.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modprf5) {
+    modprf1.style.display = "none";
+  }
+}
+
+var modprf6 = document.getElementById("modprof6");
+var profdesc6 = document.getElementById("profdesc6");
+var spanprof6 = document.getElementsByClassName("closeprof6")[0];
+
+profdesc6.onclick = function() {
+  modprf6.style.display = "block";
+}
+
+spanprof6.onclick = function() {
+  modprf6.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modprf6) {
+    modprf1.style.display = "none";
+  }
+}
